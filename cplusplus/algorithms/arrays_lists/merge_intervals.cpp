0ecc1c027d28031e7be5ff54b2cbe231e4bb46c2
abhay-venkatesh <abhayv@fb.com>
2018-06-02 20:10:17 -0700

    Setup

---
+++ cplusplus/algorithms/arrays_lists/merge_intervals.cpp
@@ -0,0 +1,11 @@
+/*
+
+TODO: Complete this.
+
+Given two lists A and B that contain non-overlapping intervals,
+merge A and B to produce a new list with non-overlapping intervals.
+
+For example,
+A = [[1,4],[5,10],[12,20]]
+
+*/
