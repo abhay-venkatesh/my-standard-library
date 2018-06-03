06412b9d2f6c0cb618e3436ac54bef04dfb063ad
abhay-venkatesh <abhayv@fb.com>
2018-06-03 11:05:58 -0700

    Implement breadth first search

--- cplusplus/algorithms/graphs/bfs.cpp
+++ cplusplus/algorithms/graphs/bfs.cpp
@@ -3,15 +3,40 @@
 
 // TODO: Complete this
 
-void bfs(vector<vector<int>> graph) {
+using namespace std;
 
+void bfs(int curr, vector<vector<int>> graph,
+         vector<vector<bool>> visited) {
+
+  for(const auto& neighbor: graph[curr]) {
+    if(visited[curr][neighbor] == false) {
+      cout << neighbor << " ";
+    }
+  }
+
+  for(const auto& neighbor: graph[curr]) {
+    if(visited[curr][neighbor] == false) {
+      visited[curr][neighbor] = true;
+      bfs(neighbor, graph, visited);
+    }
+  }
+}
+
+void bfs(vector<vector<int>> graph) {
+  vector<vector<bool>> visited(graph.size(),
+                               vector<bool>(graph.size(), 0));
+
+  bfs(0, graph, visited);
 }
 
 int main() {
   vector<vector<int>> graph = {
-    {2,3,4},
+    {1,2,3},
     {},
     {},
-    {5,6}
+    {4,5},
+    {},
+    {}
   };
+  bfs(graph);
 }
