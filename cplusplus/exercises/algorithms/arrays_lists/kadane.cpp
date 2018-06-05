70e5b8ad04874b68b089c49ba94f825515f8fbed
abhay-venkatesh <abhayv@fb.com>
2018-06-05 14:40:16 -0700

    Write kadane

--- cplusplus/exercises/algorithms/arrays_lists/kadane.cpp
+++ cplusplus/exercises/algorithms/arrays_lists/kadane.cpp
@@ -6,9 +6,32 @@ using namespace std;
 
 /*
 
+To find the maximum subarray, the brute force would
+simply be to check every possible subarray. If we search this
+by starting with the full array, and then subtracting one piece
+from each side, then we have 2^n subarrays. That is too bad.
+
+We can be smarter about this. Do we really care about trying
+every single subarray? Is there a way to exploit solutions
+to subarrays? Consider the followign input
+
 input = -1,4,3,-4,5,6,-2,6
-max_ending_here = 12
-max_so_far = 14
+
+If we have the solution to the problem for [-1,4,3,-4,5],
+can we construct a solution to [-1,4,3,-4,5,6]?
+
+The only determining factor is if we include 6 or not.
+Does that improve or worsen our solution? What if 6 itself is
+larger than the solution?
+
+There are really 2 cases,
+1) Include 6 in the running sum
+3) Take only 6
+
+since the array has to be contiguous. Now, it is possible
+that since we are taking only contiguous arrays that the running
+array becomes smaller than what is necessary. Therefore,
+we maintain a global sum as well.
 
 */
 
