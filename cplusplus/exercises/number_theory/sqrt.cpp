4bbdef09290bd0b727f6b18d0eda9edfe672f480
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-06-23 01:02:42 -0700

    Write sqrt

--- cplusplus/exercises/number_theory/sqrt.cpp
+++ cplusplus/exercises/number_theory/sqrt.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 /*
-x = 5
-left = 1
-right = 2
-mid = 1
+
+We simply search the number line for the square root.
+
+--- my_sqrt(x = 5) ---
+1 2 3 4 5
+left = 2, right = 2, mid = 2
 
 */
 int my_sqrt(int x) {
   int left = 1, right = x;
   while (left <= right) {
-    int mid = left + (right - left)/2;
+    int mid = (left + right)/2;
     if (mid == x/mid) {
       return mid;
     } else if (mid < x/mid) {
