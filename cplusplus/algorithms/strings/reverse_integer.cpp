377dede954223435076fe575ae0ce5784624cb2d
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-07-07 11:38:18 -0700

    Add string

---
+++ cplusplus/algorithms/strings/reverse_integer.cpp
@@ -0,0 +1,36 @@
+/*
+
+Let us understand the following operators well.
+
+1. Modulo: %
+Consider a number, 192481
+
+192481 % 10 = 1
+18283 % 10 = 2
+
+So % 10 basically extracts the last digit.
+$ 100 would extract the last two digits, and so on. Pretty cool
+
+Can we generalize this further?
+
+192481 % 2 = 1
+18283 % 2 = 1
+
+% 2 basically returns 1 if not even, 0 if even. Interesting.
+This is because you can use % 2 to check if a number is even or not.
+
+What about % 3?
+
+192481 % 3 = ?
+
+Hard to generalize from here on.
+
+2. Division: /
+Consider a number, 18623
+
+18623 / 10 = 1862
+18623 / 100 = 186
+
+So division by 10 truncates digits from the back.
+
+*/
