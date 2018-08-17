6374fb80a236399555a3896674652917ec09e4e9
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-08-17 08:12:15 -0700

    More work on RS

--- python/feynman/data_structures/randomized_set.py
+++ python/feynman/data_structures/randomized_set.py
@@ -1,71 +1,26 @@
 import random
 
-class RandomizedSet(object):
+class RandomizedSet:
     """
-    We want to be able to getRandom() in O(1)-time. This is easy if we can
-    index into our data structure in O(1)-time. However, in order to do that,
-    we need to be able to maintain a unique indexing over our data structure
-    in O(1)-time.
-
-    The issue is that we can easily insert, and getRandom() in O(1)-time by
-    simply incrementally attaching a number to each inserted item,
-    but then when we delete items, the deleted indexes are no longer mapped.
-
-    One thing we could do is to push the deleted indexes into a stack,
-    and then pull from the stack instead of adding a new number.
-
     count = 1
-    stack = [0]
-    table = {
-        2:1
+    vals = {
+        0: 1,
+        1: 2
     }
-    indexes = {
-        1:2
+    indices = {
+        1: 0
     }
-
-    insert(1) -> True
-    remove(2) -> False
-    insert(2) -> True
-    getRandom() -> 2
-    remove(1) -> True  
-
-    But now if we do getRandom() then we cannot avoid the deleted index
-    in O(1)-time. What if also have a getMax() in constant time, and then
-    whenever something is deleted, we replace the max number with the deleted
-    number. 
-
-    So, in this case, we would have deleted (1 -> index:0), and then, 
-    updated (2 -> index:1) to (2 -> index:0).
-
-    We can maintain a stack of max numbers that keep getting updated whenever
-    we delete. 
-
-    To recap,
-    when we insert, we update the stack with the new max number,
-    and then add an entry to the table and indexes dicts, and increment
-    the count counter.
-
-    When we delete, we replace the deleted index with the max number from the 
-    stack, and decrement the counter.
-
-    Then, when we get random by simply generating a number between
-    0 and count. 
-
-    But we don't really need a stack to maintain maxes because counter
-    actually does that for us. 
-
-    self.count = 0
-    self.table = {}
-    self.indexes = {}
-
+    
     """
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        self.table = {}
-        self.indexes = {}
         self.count = 0
+        self.vals = {}
+        self.indices = {}
+        
 
     def insert(self, val):
         """
@@ -73,14 +28,14 @@ class RandomizedSet(object):
         :type val: int
         :rtype: bool
         """
-        if val in self.table.keys():
+        if val in self.indices.keys():
             return False
-
-        self.table[val] = self.count
-        self.indexes[self.count] = val
+        
+        self.vals[self.count] = val
+        self.indices[val] = self.count
         self.count += 1
         return True
-
+        
 
     def remove(self, val):
         """
@@ -88,32 +43,27 @@ class RandomizedSet(object):
         :type val: int
         :rtype: bool
         """
-        if val not in self.table.keys():
+        if val not in self.indices.keys():
             return False
-
-        deleted_index = self.table[val]
-        del self.table[val]
-
+        
         self.count -= 1
-        val_with_max_index = self.indexes[self.count] 
-        del self.indexes[self.count]
-        self.indexes[deleted_index] = val_with_max_index
-        self.table[val_with_max_index] = deleted_index
-
+        
+        index = self.indices[val]
+        self.vals.pop(index, None)
+        self.indices.pop(val, None)
+        
+        val_ = self.vals[self.count]
+        self.indices[val_] = index
         return True
+        
 
     def getRandom(self):
         """
         Get a random element from the set.
         :rtype: int
         """
-        if self.count == 0:
-            return -1
-        if self.count == 1:
-            return self.indexes[0]
-
-        ri = random.randint(0, self.count - 1)
-        return self.indexes[ri]
+        index = random.randint(0, self.count - 1)
+        return self.vals[index]
 
 def main():
     """
