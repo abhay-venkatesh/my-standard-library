b53945937413a77567503f6127ce64cf250cdd95
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-08-17 07:24:59 -0700

    Finish

--- python/notes/algorithms/binary_search.py
+++ python/notes/algorithms/binary_search.py
@@ -10,8 +10,30 @@ def binary_search(A, target):
     if A[i] != target: return -1
     return i
 
-A = [1, 1, 2, 3, 3, 4, 5, 5, 6]
-i = binary_search(A, 3)
 print(i)
 i = binary_search(A, 12)
 print(i)
+
+"""
+--- Binary Search ---
+In order to analyze the correctness of this algorithm, we want to see
+why doing this type of searching always works. 
+
+For instance, if we do,
+A = [1, 1, 2, 3, 3, 4, 5, 5, 6]
+i = binary_search(A, 3)
+
+The loop terminates when i == j, which happens because the search interval
+keeps shrinking by half and eventually becomes size 2. When it is size 2,
+then mid == i always, and then if we found our target, we will set
+j = mid and terminate. If we didn't find our target, then we set j = mid 
+or i = mid + 1, which forces termination regardless.
+
+Basically, we define our loop invariant as: If our search target is present
+in the array A, then it is in the subarray A[i:j+1]. 
+
+At initialization this is trivially true. 
+At termination, i == j,
+therefore, the element of interest would be at A[i]. 
+
+"""
