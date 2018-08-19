60b5b1ef80cc97f8f23f13be92924fed697c58ca
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-08-18 21:58:24 -0500

    Some more work

--- python/feynman/algorithms/other/task_scheduler.py
+++ python/feynman/algorithms/other/task_scheduler.py
@@ -17,10 +17,24 @@ class Solution:
         In order to do so, we basically minimize the idle time. We can minimize idle time by 
         scheduling tasks of different types back to back. 
         
-        [A: 3, B: 3, C: 2], n = 5
-        """
+        [A: 3, B: 2, C: 1], n = 2
         
-    def tasksWithCounts(self, tasks):
+        A,B,C,A,B,_
+        
+        [A: 1, B: 2, C: 3], n = 2
+        A,B,C,_
+        """
+        tasks_with_counts = self.getTasksWithCounts(tasks)
+        tasks_with_counts.sort()
+        
+        intervals = 0
+        task_i = 0
+        while tasks_with_counts:
+            pass
+            
+        return intervals
+        
+    def getTasksWithCounts(self, tasks):
         count = 0
         prev = None
         tasks_with_counts = []
@@ -32,11 +46,11 @@ class Solution:
                 if task == prev:
                     count += 1
                 else:
-                    tasks_with_counts.append((prev, count))
+                    tasks_with_counts.append((count, task))
                     prev = task
                     count =  1
         if prev:
-            tasks_with_counts.append((prev, count))
+            tasks_with_counts.append((count, prev))
 
         return tasks_with_counts
     
