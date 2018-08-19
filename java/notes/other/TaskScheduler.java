255c4009f647adb154aaa06aaf5aab58ced84e08
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-08-19 16:42:51 -0500

    write task scheduler

--- java/notes/other/TaskScheduler.java
+++ java/notes/other/TaskScheduler.java
@@ -1,8 +1,23 @@
 public class TaskScheduler {
+    /*
+    tasks = {'A', 'B', 'C', 'A', 'A', 'B', 'A'}
+    n = 2
+    c = {..., (23 x 0), ..., 1, 2, 4}
+    i = 24
+
+    tasks.length = 7, 10
+    A, B, C, A, B, _, A, _, _, A
+    
+    --- The Frame Business ---
+    Basically, the idea is to use the most frequent task as the delimiter
+    for the frames, and then since all the other tasks are at least as
+    frequent as the most frequent task, then there will be at most
+    only one instance of them occuring.
+    */
     public int leastInterval(char[] tasks, int n) {
 
         int[] c = new int[26];
-        for(char t : tasks){
+        for(char t : tasks) {
             c[t - 'A']++;
         }
         Arrays.sort(c);
