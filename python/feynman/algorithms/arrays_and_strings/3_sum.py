e5c9f9ec9504f4acfcacc0b3a18e7f6d0c359433
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-08-23 16:58:49 -0500

    Two sum

--- python/feynman/algorithms/arrays_and_strings/3_sum.py
+++ python/feynman/algorithms/arrays_and_strings/3_sum.py
@@ -1,25 +1,13 @@
-def twoSum(self, nums, target):
-        """
-        [-1, 0, 1, 2, -1, -4]
-        
-        target = 2
-        num = 1
-        
-        table = {-1}
-        
-        num - table.get(target - num) == 0:
-            return [num, table.get(target - num)]
-    
-        """
+def twoSum(nums, target):
         bag = set()
         for num in nums:
-            bag.add(num)
-            
             if target - num in bag:
                 return [num, target - num]
                 
+            bag.add(num)
+                
 def main():
-    print(twoSum([-1, 0, 1, 2, -1, -4]))
+    print(twoSum([0, 1, -1], 0))
 
 if __name__ == '__main__':
     main()
