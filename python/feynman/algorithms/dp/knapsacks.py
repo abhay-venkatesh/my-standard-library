975e6ce282b854224fdd54f3955515549592f7e0
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-08-24 13:18:02 -0500

    add 0/1 knapsack

--- python/feynman/algorithms/dp/knapsacks.py
+++ python/feynman/algorithms/dp/knapsacks.py
@@ -43,3 +43,61 @@ If this process does not produce an optimal solution, then that would mean that
 one of the subproblems did not have an optimal solution. But by construction,
 each of the subproblems does have an optimal solution, because we have tried it.
 """
+
+"""
+--- The 0/1 Knapsack ---
+We make a slight change to the unbounded knapsack problem: the items are now
+in a finite quantity. Therefore, it is not just important to have solutions
+to smaller knapsacks, but we also need to know what items were used 
+in that solution. Therefore, we need solutions to subproblems along two
+dimensions:
+1. Items used
+2. Weight
+
+solutions = [[0 for _ in range(K+1)] for _ in range(len(W)+1)]
+
+We illustrate with a toy problem:
+W = [1, 2, 3, 4]
+V = [1, 3, 7, 15]
+K = 4
+
+solutions = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+
+solutions[i][j] is the solution to a knapsack of weight j using items 
+[-1, ..., i-1], where -1 represents no item.
+
+solutions[0][j] for all j are all 0, since knapsacks of weight 0 cannot hold anything,
+and solutions[i][0] for all i are all 0, since i = 0 represents -1, no item,
+so we cannot have any value if we cannot take any items.
+
+fix j = 1, then
+solutions[i][1] = 
+if item at i fits in knapsack:
+    solutions[i[1] = max(
+        solutions[i-1][1], 
+        value(i) + solutions[0][1 - weight(i)]
+    )
+else:
+    solutions[i][1] = solutions[i-1][1]
+
+Why does this work? A solution to subproblem[i][j] is constructed using
+only one instance of each item. This is true because of the line:
+
+solutions[1][1] = max(
+    solutions[i-1][1], 
+    value(i) + solutions[0][1 - weight(i)]
+)
+
+Why is each version of this optimal? We took each item at most once, 
+and, we took the maximum possible version of the combination of items, 
+and tried each item, and tried to fit in in the knapsack. 
+
+If this solution is wrong, then the subproblem solution is wrong, which is
+false by construction
+"""
