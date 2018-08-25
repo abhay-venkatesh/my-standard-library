48dd02c7fcf4d21b6087ae99e706ead6d6623cac
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-08-24 19:55:12 -0500

    Minimal decorator

--- python/feynman/pythonic/decorators.py
+++ python/feynman/pythonic/decorators.py
@@ -1,23 +1,13 @@
-import functools
+from functools import wraps
 
-def condition(pre_condition=None, post_condition=None):
+def pre_condition(condition):
     def decorator(func):
-        @functools.wraps(func) # presever name, docstring, etc
-        def wrapper(*args, **kwargs): #NOTE: no self
-            if pre_condition is not None:
-               assert pre_condition(*args, **kwargs)
-            retval = func(*args, **kwargs) # call original function or method
-            if post_condition is not None:
-               assert post_condition(retval)
-            return retval
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            assert condition(*args, **kwargs)
+            return func(*args, **kwargs) 
         return wrapper
     return decorator
-
-def pre_condition(check):
-    return condition(pre_condition=check)
-
-def post_condition(check):
-    return condition(post_condition=check)
 
 def three_sum(nums):
     nums.sort()
