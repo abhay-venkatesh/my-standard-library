ebd8e1b1a9201589a786f580bfa7cac39e71f1c9
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-08-24 20:47:52 -0500

    implement 2-pass 3 sum

--- python/feynman/algorithms/arrays_and_strings/3_sum.py
+++ python/feynman/algorithms/arrays_and_strings/3_sum.py
@@ -1,33 +1,64 @@
-import functools
+from functools import wraps
 
-def condition(pre_condition=None, post_condition=None):
+def pre_condition(condition):
     def decorator(func):
-        @functools.wraps(func) # presever name, docstring, etc
-        def wrapper(*args, **kwargs): #NOTE: no self
-            if pre_condition is not None:
-               assert pre_condition(*args, **kwargs)
-            retval = func(*args, **kwargs) # call original function or method
-            if post_condition is not None:
-               assert post_condition(retval)
-            return retval
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            assert condition(*args, **kwargs)
+            return func(*args, **kwargs)
         return wrapper
     return decorator
 
-def pre_condition(check):
-    return condition(pre_condition=check)
-
-def post_condition(check):
-    return condition(post_condition=check)
-
-def three_sum(nums):
-    nums.sort()
-
 @pre_condition(lambda nums, target: sorted(nums) == nums)
 def two_sum(nums, target):
-    print("YOLO")
+    """
+    nums = [-1, 0, 1, 2, 4, 6]
+    target = 2
+    
+    If we want to find the two numbers that sum upto 2,
+    we can simply walk inwards starting from both ends until
+    we hit a combination that matches 2. This works because:
+    assume that we did not find two numbers that sum to 2, but they 
+    actually were in the list. 
+
+    That would mean that we either incremented, or decremented i or j,
+    when we should not have had. 
+
+    When do we increment i or decrement j? 
+    We increment i when nums[i] + nums[j] < target. If we did not find
+    two numbers, but they did exist, that would mean that we should have
+    not incremented i when nums[i] + nums[j] < target, but that would not
+    happen because ...
+
+    The smallest possible sum is nums[0] + nums[1],
+    and the largest possible sum is nums[-1] + nums[-2]. 
+
+    Therefore, to get smaller, we decrement j, 
+    and to get bigger, we increment i. 
+
+    Consider our example, 
+    nums = [-1, 0, 1, 2, 4, 6]
+    target = 2
+
+    nums[0] + nums[5] == 5
+    nums[0] + nums[4] == 3
+
+    By incrementing i, we necessarily increase the sum, because every
+    value on the left of i is >= the value at i. 
+    """
+    i = 0
+    j = len(nums) - 1
+    while i < j: 
+        if nums[i] + nums[j] == target:
+            return [nums[i], nums[j]]
+        elif nums[i] + nums[j] > target:
+            j -= 1
+        else: 
+            i += 1
                 
 def main():
-    two_sum([0, 2, 1], 2)
+    sol = two_sum([-1, 0, 1, 2, 4, 6], -1)
+    print(sol)
 
 if __name__ == '__main__':
     main()
